a81d1d6ea2ce0da480b530766facf32f909db6be
ZIN MYO <overmidnight12@gmail.com>
2021-02-10 14:59:26 +0630

    add admin template

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required Meta Tags Always Come First -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Title -->
+    <title>Dashboard | Front - Admin &amp; Dashboard Template</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="https://htmlstream.com/front-dashboard/favicon.ico">
+
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
+
+    <!-- CSS Implementing Plugins -->
+    <link rel="stylesheet" href="https://htmlstream.com/front-dashboard/assets/css/vendor.min.css">
+    <link rel="stylesheet" href="https://htmlstream.com/front-dashboard/assets/vendor/icon-set/style.css">
+    
+    
+
+    <!-- CSS Front Template -->
+    <link rel="stylesheet" href="https://htmlstream.com/front-dashboard/assets/css/theme.min.css?v=1.0">
+  </head>
